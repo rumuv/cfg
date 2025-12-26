3900dc9aa8ae902e069ec348360183ba2b8079a0
Ruslan Muzafarov <rumuvs@gmail.com>
2025-12-26 04:39:57 +0300

    feat: add shell files (zsh/bash)

---
+++ .zshrc
@@ -0,0 +1,3 @@
+export LANG=C.UTF-8
+export LC_ALL=C.UTF-8
+export LC_CTYPE=C.UTF-8
--- setup.sh
+++ setup.sh
@@ -2,5 +2,15 @@
 
 set -eux
 
+default_shell=$(basename "$SHELL")
+
 ln -sf $PWD/.vimrc $HOME/.vimrc
 ln -sf $PWD/.tmux.conf $HOME/.tmux.conf
+
+if [ "$default_shell" == "zsh" ]; then
+  ln -sf $PWD/.zshrc $HOME/.zshrc
+elif [ "$default_shell" == "bash" ]; then
+  ln -sf $PWD/.bashrc $HOME/.bashrc
+else
+  echo "Cannot detect default user shell"
+fi
